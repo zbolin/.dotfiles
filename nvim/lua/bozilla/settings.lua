ad7d518ebafdf5e963f865da6307264650f82082
Zach Bolin <zdbolin@gmail.com>
2022-03-03 19:05:38 -0600

    [AIR] Updated init.vim

--- nvim/lua/bozilla/settings.lua
+++ nvim/lua/bozilla/settings.lua
@@ -2,11 +2,7 @@ vim.g.mapleader = " "
 
 local time = os.date("*t")
 
-
 if ( time.hour > 9 and time.hour < 11)
-then
-    vim.g.tokyonight_style = "day"
-elseif( time.hour > 15 and time.hour < 20 )
 then
     vim.g.tokyonight_style = "storm"
 else
